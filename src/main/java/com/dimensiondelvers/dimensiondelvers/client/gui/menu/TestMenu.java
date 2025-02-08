695476ebbb13faf0da718ae53ae74bafe5d56873
robotmonkey1000 <robotmonkey000@gmail.com>
2025-02-08 18:25:43 -0500

    refactor: Cleaned up unused imports

--- src/main/java/com/dimensiondelvers/dimensiondelvers/client/gui/menu/TestMenu.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/client/gui/menu/TestMenu.java
@@ -1,23 +1,12 @@
 package com.dimensiondelvers.dimensiondelvers.client.gui.menu;
 
-import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
-import com.dimensiondelvers.dimensiondelvers.abilities.AbstractAbility;
 import com.dimensiondelvers.dimensiondelvers.init.ModMenuTypes;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
-import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
-import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
-import java.util.function.Supplier;
-
-import static com.dimensiondelvers.dimensiondelvers.init.ModMenuTypes.TEST_MENU;
 
 public class TestMenu extends AbstractContainerMenu {
 
@@ -27,7 +16,7 @@ public class TestMenu extends AbstractContainerMenu {
     }
 
     public TestMenu(int containerId, Inventory playerInventory, ContainerLevelAccess access) {
-        super(TEST_MENU.get(), containerId);
+        super(ModMenuTypes.TEST_MENU.get(), containerId);
 //        this.playerInventory = playerInventory;
 //        this.access = access;
 //        this.createInventorySlots(playerInventory);
