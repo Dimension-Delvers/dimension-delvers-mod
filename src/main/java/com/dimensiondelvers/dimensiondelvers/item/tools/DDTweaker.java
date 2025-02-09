b00dd4cbd88b35ef4b525279ab107f6bd2b5e2f6
Wh4I3-2 <154924333+Wh4I3-2@users.noreply.github.com>
2025-02-09 16:47:39 +0100

    Made Spring Blocks adjustable with DD Tweaker

--- src/main/java/com/dimensiondelvers/dimensiondelvers/item/tools/DDTweaker.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/item/tools/DDTweaker.java
@@ -1,8 +1,11 @@
 package com.dimensiondelvers.dimensiondelvers.item.tools;
 
+import com.dimensiondelvers.dimensiondelvers.block.SpringBlock;
 import com.dimensiondelvers.dimensiondelvers.block.TrapBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
@@ -19,12 +22,28 @@ public class DDTweaker extends Item {
         BlockPos pos = context.getClickedPos();
         Level level = context.getLevel();
         BlockState state = level.getBlockState(pos);
+        Player player = context.getPlayer();
 
         if (state.getBlock() instanceof TrapBlock) {
             level.setBlockAndUpdate(pos, ((TrapBlock) state.getBlock()).getTweak());
             return InteractionResult.SUCCESS;
         }
+        if (state.getBlock() instanceof SpringBlock) {
+            int strength = state.getValue(SpringBlock.STRENGTH);
+            strength++;
+            if (strength > SpringBlock.MAX_STRENGTH) {
+                strength = SpringBlock.MIN_STRENGTH;
+            }
+
+            level.setBlockAndUpdate(pos, state.setValue(SpringBlock.STRENGTH, strength));
+            if (player != null) { message(player, Component.literal("strength = " + strength)); }
+            return InteractionResult.SUCCESS;
+        }
 
         return InteractionResult.PASS;
+    }
+
+    private static void message(@NotNull Player player, Component messageComponent) {
+        player.displayClientMessage(messageComponent, true);
     }
 }
