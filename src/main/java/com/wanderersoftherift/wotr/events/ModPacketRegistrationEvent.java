b51e571b88e7e45bb475f25318d1bf842b2bb980
McMelonTV <mcmelontube@gmail.com>
2025-02-14 03:29:42 +0100

    feat: create a packet for registering a world on the client

--- src/main/java/com/wanderersoftherift/wotr/events/ModPacketRegistrationEvent.java
+++ src/main/java/com/wanderersoftherift/wotr/events/ModPacketRegistrationEvent.java
@@ -2,6 +2,7 @@ package com.wanderersoftherift.wotr.events;
 
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
 import com.wanderersoftherift.wotr.network.C2SRuneAnvilApplyPacket;
+import com.wanderersoftherift.wotr.network.S2CDimensionTypesUpdatePacket;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
@@ -16,6 +17,12 @@ public class ModPacketRegistrationEvent {
                 C2SRuneAnvilApplyPacket.TYPE,
                 C2SRuneAnvilApplyPacket.STREAM_CODEC,
                 new C2SRuneAnvilApplyPacket.C2SRuneAnvilApplyPacketHandler()
+        );
+
+        registrar.playToClient(
+                S2CDimensionTypesUpdatePacket.TYPE,
+                S2CDimensionTypesUpdatePacket.STREAM_CODEC,
+                new S2CDimensionTypesUpdatePacket.S2CDimensionTypesUpdatePacketHandler()
         );
     }
 }
