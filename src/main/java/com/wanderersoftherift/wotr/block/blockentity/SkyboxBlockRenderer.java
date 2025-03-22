d27c5c0f7e34883e9d0648aec3584d8c9fe0c8dc
Wh4I3-2 <154924333+Wh4I3-2@users.noreply.github.com>
2025-03-22 11:46:33 +0100

    Support for multiple sky textures

--- src/main/java/com/wanderersoftherift/wotr/block/blockentity/SkyboxBlockRenderer.java
+++ src/main/java/com/wanderersoftherift/wotr/block/blockentity/SkyboxBlockRenderer.java
@@ -12,6 +12,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.sun.jna.platform.win32.OpenGL32;
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
+import com.wanderersoftherift.wotr.block.SkyboxBlock;
 import com.wanderersoftherift.wotr.client.ModShaders;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
@@ -22,40 +23,25 @@ import net.minecraft.client.renderer.blockentity.TheEndPortalRenderer;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.neoforge.common.NeoForgeConfig;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 import org.lwjgl.opengl.GL;
 
 @OnlyIn(Dist.CLIENT)
 public class SkyboxBlockRenderer<T extends SkyboxBlockEntity> implements BlockEntityRenderer<T> {
-	public static final ResourceLocation END_SKY_LOCATION = ResourceLocation.fromNamespaceAndPath(WanderersOfTheRift.MODID, "textures/environment/skybox.png");
-
 	private static final RenderStateShard.ShaderStateShard SKYBOX_SHADER = new RenderStateShard.ShaderStateShard(ModShaders.SKYBOX);
-
-	public static final RenderType SKYBOX_RENDERTYPE = RenderType.create(
-			WanderersOfTheRift.MODID + "_skybox",
-			DefaultVertexFormat.POSITION,
-			VertexFormat.Mode.QUADS,
-			1536,
-			false,
-			false,
-			RenderType.CompositeState.builder()
-					.setShaderState(SKYBOX_SHADER)
-					.setTextureState(RenderStateShard.MultiTextureStateShard.builder()
-							.add(END_SKY_LOCATION, true, false)
-							.build()
-					).createCompositeState(false)
-	);
-
 
 	public SkyboxBlockRenderer(BlockEntityRendererProvider.Context context) {
 	}
 
-	public void render(T p_112650_, float p_112651_, PoseStack p_112652_, MultiBufferSource p_112653_, int p_112654_, int p_112655_) {
-		Matrix4f matrix4f = p_112652_.last().pose();
+	public void render(@NotNull T blockEntity, float p_112651_, PoseStack poseStack, @NotNull MultiBufferSource buffer, int p_112654_, int p_112655_) {
+		Matrix4f matrix4f = poseStack.last().pose();
 
 		CompiledShaderProgram shader = RenderSystem.setShader(ModShaders.SKYBOX);
 		if (shader != null) {
@@ -76,18 +62,16 @@ public class SkyboxBlockRenderer<T extends SkyboxBlockEntity> implements BlockEn
 
 			shader.apply();
 		}
-		this.renderCube(p_112650_, matrix4f, p_112653_.getBuffer(this.renderType()));
+		this.renderCube(blockEntity, matrix4f, buffer.getBuffer(getRenderType(blockEntity.getBlockState().getValue(SkyboxBlock.SKY))));
 	}
 
 	private void renderCube(T blockEntity, Matrix4f pose, VertexConsumer consumer) {
-		float f = this.getOffsetDown();
-		float f1 = this.getOffsetUp();
 		this.renderFace(blockEntity, pose, consumer, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, Direction.SOUTH);
 		this.renderFace(blockEntity, pose, consumer, 0.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, Direction.NORTH);
 		this.renderFace(blockEntity, pose, consumer, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F, Direction.EAST);
 		this.renderFace(blockEntity, pose, consumer, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 0.0F, Direction.WEST);
-		this.renderFace(blockEntity, pose, consumer, 0.0F, 1.0F, f, f, 0.0F, 0.0F, 1.0F, 1.0F, Direction.DOWN);
-		this.renderFace(blockEntity, pose, consumer, 0.0F, 1.0F, f1, f1, 1.0F, 1.0F, 0.0F, 0.0F, Direction.UP);
+		this.renderFace(blockEntity, pose, consumer, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F, Direction.DOWN);
+		this.renderFace(blockEntity, pose, consumer, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, Direction.UP);
 	}
 
 	private void renderFace(
@@ -104,21 +88,38 @@ public class SkyboxBlockRenderer<T extends SkyboxBlockEntity> implements BlockEn
 			float z3,
 			Direction direction
 	) {
+		if (blockEntity.getLevel() == null) {
+			return;
+		}
+		BlockState neighbor = blockEntity.getLevel().getBlockState(blockEntity.getBlockPos().subtract(direction.getUnitVec3i().multiply(-1)));
+		if (neighbor.is(blockEntity.getBlockState().getBlock())) {
+			return;
+		}
+		if (neighbor.isSolidRender()) {
+			return;
+		}
+
 		consumer.addVertex(pose, x0, y0, z0);
 		consumer.addVertex(pose, x1, y0, z1);
 		consumer.addVertex(pose, x1, y1, z2);
 		consumer.addVertex(pose, x0, y1, z3);
 	}
 
-	protected float getOffsetUp() {
-		return 1.0F;
-	}
 
-	protected float getOffsetDown() {
-		return 0.0F;
-	}
-
-	protected RenderType renderType() {
-		return SKYBOX_RENDERTYPE;
+	public static RenderType getRenderType(SkyboxBlock.Sky sky)  {
+		return RenderType.create(
+				WanderersOfTheRift.MODID + "_skybox_" + sky.name(),
+				DefaultVertexFormat.POSITION,
+				VertexFormat.Mode.QUADS,
+				1536,
+				false,
+				false,
+				RenderType.CompositeState.builder()
+						.setShaderState(SKYBOX_SHADER)
+						.setTextureState(RenderStateShard.MultiTextureStateShard.builder()
+								.add(sky.getSkyLocation(), sky.getBlur(), false)
+								.build()
+						).createCompositeState(false)
+		);
 	}
 }
