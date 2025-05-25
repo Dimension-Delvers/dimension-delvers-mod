f6d25dacebd6a57a666f9a80c4b003d09ec08cfc
immortius <immortius@gmail.com>
2025-05-25 22:55:39 +1000

    feat: trading works

--- src/main/java/com/wanderersoftherift/wotr/block/AbilityBenchBlock.java
+++ src/main/java/com/wanderersoftherift/wotr/block/AbilityBenchBlock.java
@@ -16,6 +16,7 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -75,7 +76,7 @@ public class AbilityBenchBlock extends Block {
             AbilitySlots slots = playerInventory.player.getData(WotrAttachments.ABILITY_SLOTS);
             IItemHandler replicatedSlots = new ChangeAwareItemHandler(slots) {
                 @Override
-                public void onSlotChanged(int slot) {
+                public void onSlotChanged(int slot, ItemStack oldStack, ItemStack newStack) {
                     PacketDistributor.sendToPlayer((ServerPlayer) player,
                             new AbilitySlotsUpdatePayload(slot, slots.getStackInSlot(slot)));
                 }
