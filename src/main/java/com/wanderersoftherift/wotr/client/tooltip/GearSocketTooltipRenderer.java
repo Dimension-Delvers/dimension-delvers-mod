a5ac6ec60f205728d7a2fe213c3208348b8ad0a7
a1qs <ankiller2007.09.20@gmail.com>
2025-06-21 13:50:49 +0200

    feat: update GearSocketTooltipRenderer to support modifier colors

--- src/main/java/com/wanderersoftherift/wotr/client/tooltip/GearSocketTooltipRenderer.java
+++ src/main/java/com/wanderersoftherift/wotr/client/tooltip/GearSocketTooltipRenderer.java
@@ -153,10 +153,10 @@ public class GearSocketTooltipRenderer implements ClientTooltipComponent {
                 if (tooltip instanceof ImageComponent img) {
                     if (tier == getModifierTiers(socket).size()) {
                         component.append(ComponentUtil.wavingComponent(img.base(),
-                                TextColor.parseColor("#e0ba12").getOrThrow(), 0.125f, 0.5f));
+                                socket.modifier().get().modifier().value().getColor(), 0.125f, 0.5f));
                     } else {
                         component.append(
-                                img.base().copy().withColor(TextColor.parseColor("#e0ba12").getOrThrow().getValue()));
+                                img.base().copy().withColor(socket.modifier().get().modifier().value().getColor()));
                     }
 
                 }
