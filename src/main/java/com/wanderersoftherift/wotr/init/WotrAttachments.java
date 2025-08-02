aa2f2f0112f147410f5c710973461bbd0d7607f1
immortius <immortius@gmail.com>
2025-08-02 11:48:53 +1000

    refactor: add holder to ManaData and simplified implementation

--- src/main/java/com/wanderersoftherift/wotr/init/WotrAttachments.java
+++ src/main/java/com/wanderersoftherift/wotr/init/WotrAttachments.java
@@ -57,6 +57,6 @@ public class WotrAttachments {
     public static final Supplier<AttachmentType<EffectDisplayData>> EFFECT_DISPLAY = ATTACHMENT_TYPES
             .register("effect_display", () -> AttachmentType.builder(() -> new EffectDisplayData()).build());
     public static final Supplier<AttachmentType<ManaData>> MANA = ATTACHMENT_TYPES.register("mana",
-            () -> AttachmentType.builder(ManaData::new).serialize(ManaData.CODEC).build());
+            () -> AttachmentType.builder(ManaData::new).serialize(ManaData.getSerializer()).build());
 
 }
